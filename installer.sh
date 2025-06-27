ae9a2b620f5e641b11864f414c37687810c59365
VC365 <xdood.sav@outlook.com>
2025-06-27 10:13:10 +0330

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+DIR_APP="/usr/bin"
+DIR_CONFIG="$HOME/.config/volume-pulse"
+
+read -r -d '' config << 'EOF'
+volume_increase = 5
+max_volume = 200%
+
+middle_click_action = mixer
+
+mixer = pavucontrol
+
+use_notifications = false
+
+use_shortcuts = false
+
+EOF
+install() {
+    echo -e "${YELLOW}installing volume-pulse...${NC}"
+    if [ ! -f volume-pulse ]; then
+        echo -e "${RED} volume-pulse not found in current directory!${NC}"
+        exit 1
+    fi
+    mkdir -p "$DIR_CONFIG"
+	if [ ! -f "$DIR_CONFIG/config.conf" ]; then
+      echo "$config" > "$DIR_CONFIG/config.conf"
+	fi
+	if [ -f "$DIR_APP/volume-pulse" ]; then
+	    sudo rm "$DIR_APP/volume-pulse"
+	fi
+	sudo cp volume-pulse "$DIR_APP"
+	echo -e "${GREEN} volume-pulse Installed!${NC}"
+}
+
+uninstall() {
+    sudo rm -f "$DIR_APP/volume-pulse" &> /dev/null
+    echo -e "${GREEN} volume-pulse Deleted!${NC}"
+}
+
+if [ "$1" == "install" ]; then
+    install
+elif [ "$1" == "uninstall" ]; then
+    uninstall
+else
+    echo -e "${RED} Options $0 {install|uninstall}${NC}"
+    exit 1
+fi
