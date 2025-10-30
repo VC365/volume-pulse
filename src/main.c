308b202d4d1d3c9a9dd71980bb4d0467a39ed04f
VC365 <xdood.sav@outlook.com>
2025-10-30 10:58:09 +0330

    Update version

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ char mixer[MAX_LEN] = "pavucontrol";
 int use_notifications = 0;
 int use_shortcuts = 1;
 int use_arguments = 1;
-const gchar* version = "0.2.6";
+const gchar* version = "0.2.9";
 
 
 //#For Volume Status
